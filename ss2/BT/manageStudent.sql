4c8cf54dbd404ac1447fda3f7d7ee50107ca09bc
hoangnhan2003 <ngohoangnhan03@gmail.com>
2022-10-19 20:22:42 +0700

    update

---
+++ ss2/BT/manageStudent.sql
@@ -0,0 +1,12 @@
+create table Student(
+id int primary key,
+ `name` nvarchar(30),
+ age int 
+ );
+ create table Teacher (
+id int primary key,
+ `name` nvarchar(30),
+ age int ,
+ country nvarchar(30)
+ );
+ 
