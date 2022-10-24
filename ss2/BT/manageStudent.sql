438c1ad010cb9ec9345595ff70555489c997c54c
hoangnhan2003 <ngohoangnhan03@gmail.com>
2022-10-24 20:46:27 +0700

    update

--- ss2/BT/manageStudent.sql
+++ ss2/BT/manageStudent.sql
@@ -9,4 +9,47 @@ id int primary key,
  age int ,
  country nvarchar(30)
  );
- 
+
+ create table subject(
+	subID int not null,
+    subName nvarchar(20),
+    Credit tinyint ,
+    `status` bit not null
+ );
+
+ create table Mark(
+	MarkID int not null,
+    subID int ,
+    StudentID int ,
+    mark tinyint,
+    examTimes tinyint
+ );
+
+create table class(
+	ClassID int primary key ,
+    ClassName nvarchar(20),
+    startDate date,
+    `status` bit);
+alter table`subject`  add primary key(subID);
+alter table mark add primary key(MarkID);
+
+alter table mark add foreign key(subID) references subject(subID);
+alter table mark add foreign key(StudentID) references student(id);
+
+insert into subject 
+
+values(1,'physical',5,1),
+(2,'math',4,0),
+(3,'history',3,0);
+
+		(4,'A0622i1',2022-05-28,0);
+delete from class where classID < 4;
+
+insert into class 
+values (1,'21TCLC_DT2','2021-09-21',0),
+(2,'A0522i1','2022-05-28',1);
+use student;
+insert into class 
+values (3,'22TCLC_DT2','2022-09-21',0)
+
+    
--- ss2/BT/qliBanHang.sql
+++ ss2/BT/qliBanHang.sql
@@ -25,3 +25,28 @@ CREATE TABLE OderDetails(
     foreign key (pID) REFERENCES product(pID)
 );
 ALTER TABLE OderDetails ADD PRIMARY KEY (o_ID,pID);
+
+select O.o_ID,O.oDate,P.pPrice 
+from oderdetails OS,`order` O , product P
+where (OS.o_ID = O.o_ID)
+	and(OS.pID = P.pID);
+    
+
+select C.cID,C.cname,C.cAge,P.pID,P.pName,Pprice
+from customer C,`order` O,OderDetails OD,product P
+where (C.cID = O.cID)
+	and(O.o_ID = OD.o_ID)
+    and(OD.pID = P.pID);
+Select * from customer
+where  customer.cID = (
+select C.cID
+from customer C
+where true except Select C.cID
+from `order` O ,customer C
+where (O.cID = C.cID)
+);
+use quanlibanhang;
+select O.o_ID,O.oDate,P.Pprice * OD.odQTY as thanhtien
+from product P , OderDetails OD ,`order` O
+where (OD.pID = P.pID)
+	and(OD.o_ID = O.o_ID)
